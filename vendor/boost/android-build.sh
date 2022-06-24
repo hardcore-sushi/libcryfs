fb593c468c62c2f92b0420e80e1f77c001dd3a24
Hardcore Sushi <hardcore.sushi@disroot.org>
2022-06-24 19:52:59 +0200

    Switch to boost 1.77.0

--- vendor/boost/android-build.sh
+++ vendor/boost/android-build.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-if [ $(find build/$2/lib -name libboost_*.a |wc -l) -eq 10 ]; then
+if [ $(find build/$2/lib -name libboost_*.a 2>/dev/null |wc -l) -eq 10 ]; then
 	echo "boost already built for $2";
 	exit 0
 fi
 
 mkdir -p build && rm -rf build/$2 && cd Boost-for-Android || exit 1
 
-BOOST_TAR=boost_1_76_0.tar.bz2
+BOOST_TAR=boost_1_77_0.tar.bz2
 if [ ! -f $BOOST_TAR ]; then
-	wget -O $BOOST_TAR https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.bz2 || exit 1
+	wget -O $BOOST_TAR https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.bz2 || exit 1
 fi
 
 sha256sum -c ../checksum.txt && \
-./build-android.sh --boost=1.76.0 --arch=$2 --target-version=21 \
+./build-android.sh --boost=1.77.0 --arch=$2 --target-version=21 \
 	--with-libraries=atomic,chrono,container,date_time,exception,filesystem,serialization,system,thread \
 	$1 && \
 mv build/out/$2 ../build
