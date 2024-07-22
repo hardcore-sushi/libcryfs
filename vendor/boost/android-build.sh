cd0af7088066f870f12eceed9836bde897f1d164
Hardcore Sushi <hardcore.sushi@disroot.org>
2024-07-22 21:16:39 +0200

    libcryfs: Update Boost to 1.79.0 & allow curl and wget for download

--- vendor/boost/android-build.sh
+++ vendor/boost/android-build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ $(find build/$2/lib -name libboost_*.a 2>/dev/null |wc -l) -eq 10 ]; then
+if [ "$(find build/"$2"/lib -name libboost_*.a 2>/dev/null |wc -l)" -eq 10 ]; then
 	echo "boost already built for $2";
 	exit 0
 fi
@@ -10,16 +10,24 @@ fi
 exec 9>/var/tmp/libcryfs-boost.lock
 flock 9
 
-mkdir -p build && rm -rf build/$2 && cd Boost-for-Android
+mkdir -p build && rm -rf build/"$2" && cd Boost-for-Android
 
-BOOST_TAR=boost_1_77_0.tar.bz2
+BOOST_TAR=boost_1_79_0.tar.bz2
 if [ ! -f $BOOST_TAR ]; then
-	wget -O $BOOST_TAR https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.bz2
+	DL_URL="https://archives.boost.io/release/1.79.0/source/boost_1_79_0.tar.bz2"
+	if command -v wget; then
+		wget -O $BOOST_TAR "$DL_URL"
+	elif command -v curl; then
+		curl -fLo $BOOST_TAR "$DL_URL"
+	else
+		echo "Neither curl or wget have been found">&2
+		exit 1
+	fi
 fi
 sha256sum -c ../checksum.txt
 
-./build-android.sh --boost=1.77.0 --arch=$2 --target-version=21 \
+./build-android.sh --boost=1.79.0 --arch="$2" --target-version=21 \
 	--with-libraries=atomic,chrono,container,date_time,exception,filesystem,serialization,system,thread \
-	$1
+	"$1"
 
-mv build/out/$2 ../build
+mv build/out/"$2" ../build
